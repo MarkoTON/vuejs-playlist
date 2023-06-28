8fd191aa8c5d89f2bd7793267916e171bb6f75c9
markoton <markotoncanovic@gmail.com>
2023-06-28 16:20:39 +0200

    format

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,24 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            title: 'Vue Ninjas'
-        }
-    }
-}
+  data() {
+    return {
+      title: "Vue Ninjas",
+    };
+  },
+};
 </script>
 <style scoped>
-header{
-    background: lightgreen;
-    padding: 10px;
+header {
+  background: lightgreen;
+  padding: 10px;
 }
-h1{
-    color: #222;
-    text-align: center;
+h1 {
+  color: #222;
+  text-align: center;
 }
 </style>
