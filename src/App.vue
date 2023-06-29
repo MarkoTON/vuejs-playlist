11c4720e8df7c52f273d8a95caa5ee2b1430d00c
markoton <markotoncanovic@gmail.com>
2023-06-29 13:43:48 +0200

    add key and index

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
-    <div>
-        <show-blogs></show-blogs>
-        <list-blogs></list-blogs>
-    </div>
+  <div>
+    <show-blogs></show-blogs>
+    <list-blogs></list-blogs>
+  </div>
 </template>
 
 <script>
 // Imports
-import showBlogs from './components/showBlogs.vue';
-import listBlogs from './components/listBlogs.vue';
+import showBlogs from "./components/showBlogs.vue";
+import listBlogs from "./components/listBlogs.vue";
 
 export default {
-    components: {
-        'show-blogs': showBlogs,
-        'list-blogs': listBlogs
-    },
-    data () {
-        return {
-
-        }
-    },
-    methods: {
-
-    }
-}
+  components: {
+    "show-blogs": showBlogs,
+    "list-blogs": listBlogs,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style>
-body{
-    margin: 0;
-    font-family: 'Nunito SemiBold';
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
 }
 </style>
