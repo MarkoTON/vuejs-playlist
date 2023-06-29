ceb42f2b68cc8de7a9735a07681432a9cae16ee2
markoton <markotoncanovic@gmail.com>
2023-06-29 14:04:33 +0200

    add key and wild card rout also format

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import showBlogs from './components/showBlogs.vue';
 import singleBlog from './components/singleBlog.vue';
 
 export default [
-    { path: '/', component: showBlogs},
-    { path: '/add', component: addBlog},
-    { path: '/blog/:id', component: singleBlog}
+	{ path: '/', component: showBlogs },
+	{ path: '/add', component: addBlog },
+	{ path: '/blog/:id', component: singleBlog },
+	{ path: '*', component: showBlogs }
 ]
