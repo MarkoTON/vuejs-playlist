3eeef14b90d994ba964bd68eb4bf80148d474df3
markoton <markotoncanovic@gmail.com>
2023-06-29 14:08:31 +0200

    format, add key and fix divs

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,63 +1,71 @@
 <template>
-    <div id="show-blogs">
-        <h1>All Blog Articles</h1>
-        <input type="text" v-model="search" placeholder="search blogs" />
-        <div v-for="blog in filteredBlogs" class="single-blog">
-            <router-link v-bind:to="'/blog/' + blog.id"><h2>{{ blog.title }}</h2></router-link>
-            <article>{{ blog.content }}</article>
-        </div>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div
+      v-for="(blog, index) in filteredBlogs"
+      :key="index"
+      class="single-blog"
+    >
+      <router-link v-bind:to="'/blog/' + blog.id"
+        ><h2>{{ blog.title }}</h2></router-link
+      >
+      <article>{{ blog.content }}</article>
     </div>
+  </div>
 </template>
 
 <script>
 // Imports
-import searchMixin from '../mixins/searchMixin';
+import searchMixin from "../mixins/searchMixin";
 
 export default {
-    data () {
-        return {
-            blogs: [],
-            search: ''
+  data() {
+    return {
+      blogs: [],
+      search: "",
+    };
+  },
+  created() {
+    this.$http
+      .get("https://nn-vue-playlist.firebaseio.com/posts.json")
+      .then(function (data) {
+        return data.json();
+      })
+      .then(function (data) {
+        var blogsArray = [];
+        for (let key in data) {
+          data[key].id = key;
+          blogsArray.push(data[key]);
         }
-    },
-    created() {
-        this.$http.get('https://nn-vue-playlist.firebaseio.com/posts.json').then(function(data){
-            return data.json()
-        }).then(function(data){
-            var blogsArray = [];
-            for (let key in data){
-                data[key].id = key;
-                blogsArray.push(data[key]);
-            }
-            this.blogs = blogsArray;
-            //console.log(this.blogs);
-        });
-    },
-    mixins: [searchMixin]
-}
+        this.blogs = blogsArray;
+        //console.log(this.blogs);
+      });
+  },
+  mixins: [searchMixin],
+};
 </script>
 
 <style scoped>
-#show-blogs{
-    max-width: 800px;
-    margin: 0px auto;
+#show-blogs {
+  max-width: 800px;
+  margin: 0px auto;
 }
-.single-blog{
-    padding: 20px;
-    margin: 20px 0;
-    box-sizing: border-box;
-    background: #eee;
-    border: 1px dotted #aaa;
-}
-
-#show-blogs a{
-    color: #444;
-    text-decoration: none;
-}
-input[type="text"]{
-    padding: 8px;
-    width: 100%;
-    box-sizing: border-box;
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+  border: 1px dotted #aaa;
 }
 
+#show-blogs a {
+  color: #444;
+  text-decoration: none;
+}
+input[type="text"] {
+  padding: 8px;
+  width: 100%;
+  box-sizing: border-box;
+}
 </style>
