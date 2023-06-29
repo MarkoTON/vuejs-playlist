a4ec83b0d26650b41e94b20722f1cf9b2e67cba2
markoton <markotoncanovic@gmail.com>
2023-06-29 12:40:00 +0200

    format

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-    <div>
-        <add-blog></add-blog>
-    </div>
+  <div>
+    <add-blog></add-blog>
+  </div>
 </template>
 
 <script>
 // Imports
-import addBlog from './components/addBlog.vue';
+import addBlog from "./components/addBlog.vue";
 
 export default {
-    components: {
-        'add-blog': addBlog
-    },
-    data () {
-        return {
-
-        }
-    },
-    methods: {
-
-    }
-}
+  components: {
+    "add-blog": addBlog,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 
 <style>
-body{
-    margin: 0;
-    font-family: 'Nunito SemiBold';
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
 }
 </style>
