ee125ba98a7aca64d3d7b2bd20667bfacc4c9cd7
markoton <markotoncanovic@gmail.com>
2023-06-29 13:30:46 +0200

    format

--- src/main.js
+++ src/main.js
@@ -7,26 +7,26 @@ Vue.use(VueResource);
 
 // Custom directives
 Vue.directive('rainbow', {
-    bind(el, binding, vnode){
-        // console.log(Math.random());
-        // console.log(Math.random().toString(16));
-        // console.log(Math.random().toString(16).slice(2, 8));
-        el.style.color = "#" + Math.random().toString(16).slice(2, 8);
-    }
+  bind(el, binding, vnode) {
+    // console.log(Math.random());
+    // console.log(Math.random().toString(16));
+    // console.log(Math.random().toString(16).slice(2, 8));
+    el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+  }
 });
 
 Vue.directive('theme', {
-    bind(el, binding, vnode){
-        if (binding.value == 'wide'){
-            el.style.maxWidth = "1260px";
-        } else if (binding.value = 'narrow'){
-            el.style.maxWidth = "560px";
-        }
-        if(binding.arg == 'column'){
-            el.style.background = '#ddd';
-            el.style.padding = '50px';
-        }
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1260px";
+    } else if (binding.value = 'narrow') {
+      el.style.maxWidth = "560px";
     }
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '50px';
+    }
+  }
 });
 
 new Vue({
